61d858309952b7aa893451d4118b4ff29630a192
Vadim <mr.kucheev@gmail.com>
2025-02-10 06:38:24 +0300

    backend functions

--- myproject/main/views.py
+++ myproject/main/views.py
@@ -3,6 +3,7 @@ from .datahook import datahook_lib
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 
+
 def check_login(admin_req=False):
     def decorator(func):
         def wrapper(*args, **kwargs):
@@ -18,11 +19,13 @@ def check_login(admin_req=False):
         return wrapper
     return decorator
 
+
 @check_login()
 def main(request):
     context = {}
     context['inventory'] = datahook_lib.fetch_inventory()
     return render(request, 'inv/main.html', context=context)
+
 
 def index(request):
     if not request.session.session_key:
@@ -36,17 +39,21 @@ def index(request):
             return redirect('/')
     return render(request, 'inv/index.html')
 
+
 @check_login()
 def orders(request):
     context = {}
     context['sentreqs'] = datahook_lib.fetch_sent_requests(request.session.session_key, request.META['REMOTE_ADDR'])
     return render(request, 'inv/orders.html', context=context)
 
+
 @check_login()
 def profile(request):
     context = {}
     context['bels'] = datahook_lib.fetch_my_belongings(request.session.session_key, request.META['REMOTE_ADDR'])
+    context['reqs'] = datahook_lib.fetch_sent_requests(request.session.session_key, request.META['REMOTE_ADDR'])
     return render(request, 'inv/profile.html', context=context)
+
 
 @check_login(admin_req=True)
 def admin(request):
@@ -58,33 +65,89 @@ def admin(request):
     context['report'] = datahook_lib.create_report()
     return render(request, 'inv/admin.html', context=context)
 
+
 def test(request):
     context = {}
     context = datahook_lib.fetch_inventory()
     print(context)
     return render(request, 'inv/test.html', context=context)
 
+
 def logout(request):
     datahook_lib.end_session(request.session.session_key, request.META['REMOTE_ADDR'])
     return redirect('/index/')
 
+
 @check_login()
 def create_request(request):
-    print(request.POST)
     datahook_lib.create_request('to_pin_element', request.POST['item_id'], int(request.POST['quantity']), request.session.session_key, request.META['REMOTE_ADDR'])
     return redirect('/')
 
-@check_login(True)
-def approve_request(request, id):
-    datahook_lib.approve_request(id)
-    return redirect('/admin/')
 
 @check_login(True)
-def reject_request(request, id):
-    datahook_lib.decline_request(id)
+def approve_request(request):
+    datahook_lib.approve_request(request.POST['id'])
     return redirect('/admin/')
+
+
+@check_login(True)
+def reject_request(request):
+    datahook_lib.decline_request(request.POST['id'])
+    return redirect('/admin/')
+
 
 @check_login()
-def return_item(request, id):
-    datahook_lib.return_my_belonging(id, request.session.session_key, request.META['REMOTE_ADDR'])
+def return_item(request):
+    datahook_lib.return_my_belonging(request.POST['id'], request.session.session_key, request.META['REMOTE_ADDR'])
     return redirect('/profile/')
+
+
+def cancel_request(request):
+    datahook_lib.cancel_request(request.POST['id'], request.session.session_key, request.META['REMOTE_ADDR'])
+    return redirect('/profile/')
+
+
+@check_login(True)
+def edit_item(request):
+    data = request.POST
+    datahook_lib.edit_inventory_object(data['id'], data['name'], data['amount'], data['status'], request.session.session_key, request.META['REMOTE_ADDR'])
+    return redirect('/admin/')
+
+
+@check_login(True)
+def add_item(request):
+    data = request.POST
+    datahook_lib.add_to_inventory(data['name'], data['amount'], data['status'], request.session.session_key, request.META['REMOTE_ADDR'])
+    return redirect('/admin/')
+
+
+@check_login(True)
+def delete_item(request):
+    datahook_lib.remove_from_inventory(request.POST['id'], request.session.session_key, request.META['REMOTE_ADDR'])
+    return redirect('/admin/')
+
+
+@check_login(True)
+def add_plan(request):
+    data = request.POST
+    datahook_lib.add_to_plan(data['name'], data['price'], data['amount'], data['supplier'])
+    return redirect('/admin/')
+
+
+@check_login(True)
+def delete_plan(request):
+    datahook_lib.remove_from_plan(request.POST['id'])
+    return redirect('/admin/')
+
+
+@check_login(True)
+def add_user(request):
+    data = request.POST
+    datahook_lib.register_user(data['username'], data['firstname'], data['password'], data['secondname'])
+    return redirect('/admin/')
+
+
+@check_login(True)
+def assign_admin(request):
+    datahook_lib.assign_administrator(request.POST['username'])
+    return redirect('/admin/')
