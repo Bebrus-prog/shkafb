8b8eaa32e33c08a1c4d18385773870287e9e82da
Bebrus-prog <mr.pingvinchic@gmail.com>
2025-02-11 21:30:07 +0300

    congratulations!

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^m-4_j+5o#2oodbd5p1l@ehtjoszz%p(#)^+(liw75j^d(8y-p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['shkafb.ru', 'www.shkafb.ru', '0.0.0.0', '192.168.2.4', '127.0.0.1']
 
